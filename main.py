2c0b276cc722371f42e6e96dd414e9336904377e
Wolfgang <wolf_leclair7@tutanota.com>
2016-08-31 01:57:46 -0500

    put some logic in the prototype functions

--- main.py
+++ main.py
@@ -42,54 +42,52 @@ ambiguity is an issue though
 
 '''
 
+'''
 import time
 from gensim.models import Word2Vec
 
-model = Word2Vec.load(path/to/your/model)
-
+model = Word2Vec.load()
 model.similarity('apple', 'orange')
-
+'''
 
 modes = ["wordToDefinition", "definitionToWord", "createRelation"]
-
 mode = "wordToDefinition"
-
 questionAnswerDict = {"route flapping": "neighbors to switch to new routes and advertise them to their neighbor",
                       "BGP Wedgie": "ability of Charlie to force a BGP session reset can allow the configuration" + \
                                     " of Alice or Bob to transition into a stable but undesired forwarding state"}
 
 
+
+# a first step to question answering logic
 from nltk.corpus import wordnet as wn
-
 import time
+import hammmingDistance
+import timeQuestionAnswering
 
-'''
-w1 = 'boat'
-w2 = 'frog'
-w3 = 'cat'
-w4 = 'penis'
+def wordNetSimilarityTest():
+    w1 = 'boat'
+    w2 = 'frog'
+    w3 = 'cat'
+    w4 = 'boat'
 
-s1 = wn.synsets(w1)
-s2 = wn.synsets(w2)
-s3 = wn.synsets(w3)
-s4 = wn.synsets(w4)
+    s1 = wn.synsets(w1)
+    s2 = wn.synsets(w2)
+    s3 = wn.synsets(w3)
+    s4 = wn.synsets(w4)
+
+    now = time.time()
+    ss1 = s1[0]
+    ss2 = s2[0]
+    ss3 = s3[0]
+    ss4 = s4[0]
+
+    print( ss1.path_similarity(ss2) )
+    print( ss1.path_similarity(ss4) )
+    print("Elapsed time: " + "%.02f" % (time.time() - now ) )
+
+#################################################################
 
 
-now = time.time()
-ss1 = s1[0]
-ss2 = s2[0]
-ss3 = s3[0]
-ss4 = s4[0]
-
-print( ss1.path_similarity(ss2) )
-print( ss1.path_similarity(ss4) )
-print("Elapsed time: " + "%.02f" % (time.time() - now ) )
-
-'''
-
-
-'''
-curr = time.time()
 def lemmalist(str):
     syn_set = []
     for synset in wn.synsets(str):
@@ -97,47 +95,54 @@ def lemmalist(str):
             syn_set.append(item)
     return syn_set
 
+curr = time.time()
 print( lemmalist("brain") )
 print("Yeah it took " + "%.02f" % (time.time() - curr) + " seconds.")
-'''
 
 
 '''
 It is good practice to use the with keyword when dealing with file objects. This has the advantage that the file is
 properly closed after its suite finishes, even if an exception is raised on the way. It is also much shorter than
-writing equivalent try-finally blocks:
+writing equivalent try-finally blocks
+
+Idea: to somehow take memory of last session's progress and use that logic to choose questions for this one
 '''
 
+#with open('workfile', 'r') as f:
+#    for line in f:
 
 
-with open('workfile', 'r') as f:
-    for line in f:
-
-
-
-
-for question in questionAnswerDict.keys():
+def doQuiz(questionAnswerDict):
     '''
-    this block does 1 question
+    this is the main controller so to speak, although right now it's a motley of things
+    and no logic,
+    madness before method
+
+    :param questionAnswerDict:
+    :return: None
     '''
+    for question in questionAnswerDict.keys():
+        '''
+        this block does 1 question
+        '''
+        apprenticeIn = ""
+        QuestionAnswered = False
+        questionNumber = 1
 
-    apprenticeIn = ""
-    QuestionAnswered = False
-    questionNumber = 1
+        #print("pre Answer now: " + "%.02f" % now)
+        while not QuestionAnswered:
+            now = time.time()
+            print("Prompt: " + question)
+            apprenticeIn = input("A: ")
 
-    #print("pre Answer now: " + "%.02f" % now)
-    while not QuestionAnswered:
-        now = time.time()
-        print("Prompt: " + question)
-        apprenticeIn = input("A: ")
+            # call a answer verify function implementing criteria here
+            if apprenticeIn == questionAnswerDict[question]:
+                print("Right " + "%.02f" % now)
 
-        # call a answer verify function implementing criteria here
-        if apprenticeIn == questionAnswerDict[question]:
-            print("Right " + "%.02f" % now)
-
-        aSecondTime = time.time()
-        print("It toook " + "%.02f" % (aSecondTime-now) + " to enter the answer")
+            aSecondTime = time.time()
+            print("It toook " + "%.02f" % (aSecondTime-now) + " to enter the answer")
 
 
 
-
+if __name__=='__main__':
+    wordNetSimilarityTest()
